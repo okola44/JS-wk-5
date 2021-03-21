fd8bf5680a7bcd9feb23128ed3cd1149c1258773
Okola44 <mercyimaliokola@gmail.com>
2021-03-21 11:44:49 +0300

    else statements

---
+++ practise.js
@@ -0,0 +1,30 @@
+getGrade();
+function getGrade(marks){ 
+    var marks=20;  
+if (marks>=80){  
+console.log("A");
+}else if(marks>=70){
+console.log("B");
+}else if(marks>=60){
+console.log("C");
+}else if(marks>=50){
+console.log("D");
+}else if(marks<=49){
+console.log("E");
+}else{
+    console.log("F");
+}}
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
